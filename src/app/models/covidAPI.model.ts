2d45c9b8bc0ed50aa81964076a0f1abc377488ea
Hector Quintero <isc.quch@gmail.com>
2020-04-24 17:11:53 -0500

    feat: update API models for the new endpoint
    COVID MX

--- src/app/models/covidAPI.model.ts
+++ src/app/models/covidAPI.model.ts
@@ -1,12 +1,23 @@
-export interface Country {
+export interface CountryBase {
   Country: string;
+  ISO2: string;
+  Slug: string;
+}
+export interface Country extends CountryBase {
+  Active?: number;
+  City?: string;
+  CityCode?: string;
+  Confirmed?: number;
   CountryCode?: string;
   Date?: string;
-  ISO2?: string;
+  Deaths?: number;
+  Lat?: string;
+  Lon?: string;
   NewConfirmed?: number;
   NewDeaths?: number;
   NewRecovered?: number;
-  Slug: string;
+  Province?: string;
+  Recovered?: number;
   TotalConfirmed?: number;
   TotalDeaths?: number;
   TotalRecovered?: number;
