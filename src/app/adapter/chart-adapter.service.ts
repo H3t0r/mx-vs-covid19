6bed6b67c92e6035ba57fbfdb76fe41360fb8ab3
Hector Quintero <isc.quch@gmail.com>
2020-04-25 08:35:32 -0500

    feat: transform API data to chart data
    COVID MX

--- src/app/adapter/chart-adapter.service.ts
+++ src/app/adapter/chart-adapter.service.ts
@@ -3,7 +3,7 @@ import { map } from 'rxjs/operators';
 
 import { Covid19APIService } from '../api/covid19-api.service';
 import { Country } from '../models/covidAPI.model';
-import { SingleSeries } from '@swimlane/ngx-charts';
+import { SingleSeries, MultiSeries } from '@swimlane/ngx-charts';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -24,6 +24,51 @@ export class ChartAdapterService {
     );
   }
 
+  getProgressInMexico(): Observable<MultiSeries> {
+    const to = this.getTodaysDate();
+    const from = this.getLastMonthsDate();
+
+    return this.covidAPI
+      .getByCountryAllStatus('mexico', from, to)
+      .pipe(
+        map((list) =>
+          list.reduce(this.transformToChartData, this.getProgressInitialState())
+        )
+      );
+  }
+  private transformToChartData(state: MultiSeries, item: Country): MultiSeries {
+    const {
+      Confirmed: cValue,
+      Date: date,
+      Deaths: dValue,
+      Recovered: rValue,
+    } = item;
+    const [confirmed, deaths, recovered] = state;
+
+    confirmed.series.push({ name: date, value: cValue });
+    deaths.series.push({ name: date, value: dValue });
+    recovered.series.push({ name: date, value: rValue });
+
+    return state;
+  }
+
+  private getTodaysDate(): string {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    return today.toISOString().split('.')[0] + 'Z';
+  }
+
+  private getLastMonthsDate(): string {
+    const today = new Date();
+    const lastMonth = new Date();
+
+    lastMonth.setMonth(today.getMonth() - 1);
+    lastMonth.setHours(0, 0, 0, 0);
+
+    return lastMonth.toISOString().split('.')[0] + 'Z';
+  }
+
   private getSingleSeries(country: Country, suffix: string): SingleSeries {
     const { TotalConfirmed, TotalDeaths, TotalRecovered } = country;
 
@@ -39,6 +84,23 @@ export class ChartAdapterService {
       {
         name: `Recovered (${suffix})`,
         value: TotalRecovered,
+      },
+    ];
+  }
+
+  private getProgressInitialState(): MultiSeries {
+    return [
+      {
+        name: 'Confirmed',
+        series: [],
+      },
+      {
+        name: 'Deaths',
+        series: [],
+      },
+      {
+        name: 'Recovered',
+        series: [],
       },
     ];
   }
