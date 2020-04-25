40992a820d78c10e101a92e496d82c7bca72cf33
Hector Quintero <isc.quch@gmail.com>
2020-04-25 07:52:57 -0500

    feat: add endpoint getByCountryAllStatus
    COVID MX

--- src/app/api/covid19-api.service.ts
+++ src/app/api/covid19-api.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 
-import { Summary } from '../models/covidAPI.model';
+import { Summary, Countries } from '../models/covidAPI.model';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../environments/environment';
@@ -19,5 +19,13 @@ export class Covid19APIService {
     return this.http
       .get<Summary>(this.baseURL + 'summary')
       .pipe(map((summary) => summary));
+  }
+
+  getByCountryAllStatus(countrySlug, from, to): Observable<Countries> {
+    const params = new HttpParams().set('from', from).set('to', to);
+
+    return this.http
+      .get<Countries>(`${this.baseURL}/country/${countrySlug}`, { params })
+      .pipe(map((countries) => countries));
   }
 }
